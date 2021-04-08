b67f1969e6fa7976de8e8138d90c73058f715a8b
Project-Kingdom <kartikgupta673@gmail.com>
2021-04-08 19:04:42 +0530

    Employee Records

---
+++ modules/upload.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/employee', {useNewUrlParser: true});
+var conn =mongoose.Collection;
+
+var uploadSchema =new mongoose.Schema({
+	name: String,
+	imagename: String,
+
+});
+
+var uploadModel = mongoose.model('uploadimage', uploadSchema);
+module.exports=uploadModel;
